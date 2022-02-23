c67774231f2ee4cde71b0647053ff8b17e40b680
Dominik Heller <kilianmandscharo@gmail.com>
2022-02-23 20:08:48 +0100

    added animation to the menu

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,112 +1,147 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-// import pathLogo from "./images/path.png";
-// import sortLogo from "./images/sort.png";
-// import lifeLogo from "./images/life.png";
-// import sudokuLogo from "./images/sudoku.png";
 
 const Menu = () => {
     const [menuItem, setMenuItem] = useState(1);
+    const [direction, setDirection] = useState("");
+    const [visible, setVisible] = useState([1, 2]);
+    console.log(direction);
+
+    const animate = (direction: "left" | "right") => {
+        if (direction === "left") {
+            setVisible([menuItem, menuItem === 1 ? 4 : menuItem - 1]);
+            setDirection("-");
+            setMenuItem((prev) => (prev > 1 ? prev - 1 : 4));
+        }
+        if (direction === "right") {
+            setVisible([menuItem, menuItem === 4 ? 1 : menuItem + 1]);
+            setDirection("+");
+            setMenuItem((prev) => (prev < 4 ? prev + 1 : 1));
+        }
+    };
+
+    const determineSelectorClass = (menuNumber: number) => {
+        let name = "selector";
+        if (direction !== "") {
+            if (menuNumber === visible[0]) {
+                name +=
+                    direction === "+"
+                        ? " selector-to-right-primary"
+                        : " selector-to-left-primary";
+            }
+            if (menuNumber === visible[1]) {
+                name +=
+                    direction === "+"
+                        ? " selector-to-right-secondary"
+                        : " selector-to-left-secondary";
+            }
+        }
+        return name;
+    };
+
     return (
         <div>
             <h2 className="header">Visualizing Algorithms</h2>
             <div className="app-selection">
                 <div
                     className="left-selector"
-                    onClick={() =>
-                        setMenuItem((prev) => (prev > 1 ? prev - 1 : 4))
-                    }
+                    onClick={() => animate("left")}
                 ></div>
                 <div
                     className="right-selector"
-                    onClick={() =>
-                        setMenuItem((prev) => (prev < 4 ? prev + 1 : 1))
-                    }
+                    onClick={() => animate("right")}
                 ></div>
-                {menuItem === 1 && (
-                    <Link to="/Pathfinder" style={{ textDecoration: "none" }}>
-                        <div className="selector">
-                            <h2 className="selector-label">
-                                Pathfinding Algorithms
-                            </h2>
-                            <img
-                                src="/path.png"
-                                className="image"
-                                alt="Pathfinding Logo"
-                            />
-                            <p className="selector-description">
-                                Visualize different algorithms that try to find
-                                a path between two points in a grid. Generate
-                                mazes or draw the walls yourself to see how the
-                                algorithms deal with obstacles and how they
-                                differ in their approach to get from start to
-                                destination.
-                            </p>
-                        </div>
-                    </Link>
-                )}
-
-                {menuItem === 2 && (
-                    <Link to="/sorter" style={{ textDecoration: "none" }}>
-                        <div className="selector">
-                            <h2 className="selector-label">
-                                Sorting Algorithms
-                            </h2>
-                            <img
-                                src="/sort.png"
-                                className="image"
-                                alt="Sorting Logo"
-                            />
-                            <p className="selector-description">
-                                Choose between different sorting algorithms –
-                                from bubble sort to quick sort – to see how
-                                these different algorithms go about sorting an
-                                array of numbers, with adjustable sorting speed.
-                            </p>
-                        </div>
-                    </Link>
-                )}
-                {menuItem === 3 && (
-                    <Link to="/life" style={{ textDecoration: "none" }}>
-                        <div className="selector">
-                            <h2 className="selector-label">
-                                Conway's Game of Life
-                            </h2>
-                            <img
-                                src="/life.png"
-                                className="image"
-                                alt="Game of life logo"
-                            />
-                            <p className="selector-description">
-                                John Conway's famous Game of Life (1970) – a
-                                cellular automaton – visualizes how a set of
-                                simple rules can lead to complex behavior. Input
-                                a certain state and observe how each
-                                evolutionary cycle transforms the current state
-                                of the cells.
-                            </p>
-                        </div>
-                    </Link>
-                )}
-                {menuItem === 4 && (
-                    <Link to="/sudoku" style={{ textDecoration: "none" }}>
-                        <div className="selector">
-                            <h2 className="selector-label">Sudoku Solver</h2>
-                            <img
-                                src="/sudoku.png"
-                                className="image"
-                                alt="Sudoku logo"
-                            />
-                            <p className="selector-description">
-                                Input an unfinished Sudoku into the Sudoku
-                                solver and watch it search for a correct
-                                solution. It does so by utilizing the
-                                backtracking algorithm, that is, it searches for
-                                a solution by trial-and-error.
-                            </p>
-                        </div>
-                    </Link>
-                )}
+                <div className="selector-container">
+                    {visible.includes(2) && (
+                        <Link to="/sorter" style={{ textDecoration: "none" }}>
+                            <div className={determineSelectorClass(2)}>
+                                <h2 className="selector-label">
+                                    Sorting Algorithms
+                                </h2>
+                                <img
+                                    src="/sort.png"
+                                    className="image"
+                                    alt="Sorting Logo"
+                                />
+                                <p className="selector-description">
+                                    Choose between different sorting algorithms
+                                    – from bubble sort to quick sort – to see
+                                    how these different algorithms go about
+                                    sorting an array of numbers, with adjustable
+                                    sorting speed.
+                                </p>
+                            </div>
+                        </Link>
+                    )}
+                    {visible.includes(3) && (
+                        <Link to="/life" style={{ textDecoration: "none" }}>
+                            <div className={determineSelectorClass(3)}>
+                                <h2 className="selector-label">
+                                    Conway's Game of Life
+                                </h2>
+                                <img
+                                    src="/life.png"
+                                    className="image"
+                                    alt="Game of life logo"
+                                />
+                                <p className="selector-description">
+                                    John Conway's famous Game of Life (1970) – a
+                                    cellular automaton – visualizes how a set of
+                                    simple rules can lead to complex behavior.
+                                    Input a certain state and observe how each
+                                    evolutionary cycle transforms the current
+                                    state of the cells.
+                                </p>
+                            </div>
+                        </Link>
+                    )}
+                    {visible.includes(4) && (
+                        <Link to="/sudoku" style={{ textDecoration: "none" }}>
+                            <div className={determineSelectorClass(4)}>
+                                <h2 className="selector-label">
+                                    Sudoku Solver
+                                </h2>
+                                <img
+                                    src="/sudoku.png"
+                                    className="image"
+                                    alt="Sudoku logo"
+                                />
+                                <p className="selector-description">
+                                    Input an unfinished Sudoku into the Sudoku
+                                    solver and watch it search for a correct
+                                    solution. It does so by utilizing the
+                                    backtracking algorithm, that is, it searches
+                                    for a solution by trial-and-error.
+                                </p>
+                            </div>
+                        </Link>
+                    )}
+                    {visible.includes(1) && (
+                        <Link
+                            to="/Pathfinder"
+                            style={{ textDecoration: "none" }}
+                        >
+                            <div className={determineSelectorClass(1)}>
+                                <h2 className="selector-label">
+                                    Pathfinding Algorithms
+                                </h2>
+                                <img
+                                    src="/path.png"
+                                    className="image"
+                                    alt="Pathfinding Logo"
+                                />
+                                <p className="selector-description">
+                                    Visualize different algorithms that try to
+                                    find a path between two points in a grid.
+                                    Generate mazes or draw the walls yourself to
+                                    see how the algorithms deal with obstacles
+                                    and how they differ in their approach to get
+                                    from start to destination.
+                                </p>
+                            </div>
+                        </Link>
+                    )}
+                </div>
                 <div className="indicator-section">
                     <div
                         className="indicator"
